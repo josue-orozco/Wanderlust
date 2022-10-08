e5a22da4fa6dc2f2ec94105163ce523f8fac38f2
josue-orozco <56812868+josue-orozco@users.noreply.github.com>
2022-10-08 16:45:40 -0700

    Adding initial calls to YouTube API

--- Wanderlust/Components/EmbeddedVideoViewComponent.cs
+++ Wanderlust/Components/EmbeddedVideoViewComponent.cs
@@ -1,25 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Wanderlust.Models;
 using Wanderlust.ViewModels;
 
-namespace Wanderlust.Controllers
+namespace Wanderlust.Components
 {
-    public class HomeController : Controller
+    public class EmbeddedVideoViewComponent : ViewComponent
     {
         private readonly IVideoRepository _videoRepository;
-        
-        public HomeController(IVideoRepository videoRepository)
+        public EmbeddedVideoViewComponent(IVideoRepository videoRepository)
         {
             _videoRepository = videoRepository;
         }
 
-        public IActionResult Index()
+        public IViewComponentResult Invoke()
         {
             Random rnd = new Random();
             Task<IEnumerable<string>> videos = _videoRepository.GetVideo();
@@ -31,19 +28,8 @@ namespace Wanderlust.Controllers
                 VideoUrl = videos.Result.ElementAt(rnd.Next(0, videos.Result.Count()))
             };
 
-            // cant pass it here since this page is loaded only once. will have to do an ajax call when user inputs length or maybe we select at certain increments
-            // adding video length
-            ViewBag.VideoLength = 20000;
-
             return View(homeViewModel);
         }
 
-        public IActionResult ReloadEvents()
-        {
-            Random rnd = new Random();
-            //ViewBag.VideoLength = rnd.Next(1, 6) * 10000;
-            ViewBag.VideoLength = 10000;
-            return ViewComponent("EmbeddedVideo");
-        }
     }
 }
---
+++ Wanderlust/Models/MockVideoRepository.cs
@@ -0,0 +1,84 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Wanderlust.Models
+{
+    public class MockVideoRepository : IVideoRepository
+    {
+        private string baseUrl = "https://www.youtube.com/embed/";
+
+        // Append to end of url so autoplay is enabled.
+        // Youtube has rules/conditions where videos cannot be autoplayed with sound unless the webpage is interacted with first
+        // Workaround is to autoplay video muted.
+        // Future Options:
+        // 1)   A video can start with sound if the user has interacted with the domain (click, tap, etc.) so have a loading page
+        //      introducing webpage and then have to click on something to enter which in turn will start videos with sound
+        // 2)   Start webpage as mute and then inform user to unmute with a popup
+        private string setAutoplay = "?&autoplay=1";
+        private string setAutoplayMuted = "?&autoplay=1&mute=1";
+
+        public async Task<IEnumerable<string>> GetVideo()
+        {
+            IEnumerable<string> videos;
+            
+            try
+            {
+                Task<IEnumerable<string>> getVideoTask = Run();
+                videos = await getVideoTask;
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Debug.WriteLine("Error: " + e.Message);
+                }
+
+                // Add Rick Roll Video
+                List<string> rickRoll = new List<string> { "https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" };
+                videos = rickRoll;
+            }
+
+            return videos;
+        }
+
+        private async Task<IEnumerable<string>> Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+
+                ApplicationName = this.GetType().ToString()
+            });
+
+            // Add filters to search here
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Toyota Tacoma";
+            searchListRequest.MaxResults = 5;
+            searchListRequest.Type = "video";
+            searchListRequest.VideoEmbeddable = SearchResource.ListRequest.VideoEmbeddableEnum.True__;
+            searchListRequest.SafeSearch = SearchResource.ListRequest.SafeSearchEnum.Strict;
+
+            // Add to set video location request
+            //searchListRequest.Location = "";
+            //searchListRequest.LocationRadius = "";
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+
+            // Adding results to a list as url
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                videos.Add(String.Format("{0}{1}{2}", this.baseUrl, searchResult.Id.VideoId, this.setAutoplay));
+            }
+
+            Debug.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+
+            return videos;
+        }
+    }
+}
